8e1e43c1b11f031536031737a5dc648b4a349aa5
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2022-07-16 15:43:12 +0530

    Added UC6

--- EmpWage6.java
+++ EmpWage6.java
@@ -1,51 +1,23 @@
+
 package com.empwage.program;
 
-class EmpWageUC7 {
-	public static final  int IS_FULL_TIME =1;
-	public static final int IS_PART_TIME =2;
-	public static	int EMP_RATE_PER_HOUR;
-	public static	int NUMB_WORKING_DAY;
-	public static int MAX_HR_IN_MONTH;
+class EmpWageUC6 extends EmpWageUC5 {
 
-	public EmpWageUC7(String name,int emprateperhr,int numbofworkingdays,
-			int maxhrinmonth) {  // Parameterized Constructor 
-		EMP_RATE_PER_HOUR=emprateperhr;
-		NUMB_WORKING_DAY=numbofworkingdays;
-		MAX_HR_IN_MONTH=maxhrinmonth;
-		System.out.println("The Name of Company is  "+name);
-	}
-
-	public static void main(String[] args) {
-		EmpWageUC7 Dmart= new EmpWageUC7("Dmart",20,20,100);
-		// Variables
-		int emphrs = 0;
-		int empwage = 0;
-		int totalempwage = 0;
-		int totalworkingdays = 0;
-		int totalemphrs = 0;
-		// Computation
-		while (totalemphrs < MAX_HR_IN_MONTH && totalworkingdays< NUMB_WORKING_DAY ){
-			totalworkingdays++;
-			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
-
-			switch ((int)empcheck ) {
-			case  IS_FULL_TIME :
-				emphrs = 8;
-				break;
-			case IS_PART_TIME :
-				emphrs = 4;
-				break;
-			default:
-				emphrs = 0;
-			}
-			empwage = emphrs * EMP_RATE_PER_HOUR ;
-			totalempwage += empwage;
-
-			System.out.println( "The salary of Employe is" + empwage );
+	public final int workinghours=100;
+	public int employeewages=0
+			;
+	public void EmpWageUC6() {
+		EmpWageUC4 newcase=new EmpWageUC4();
+		while (n < workingdays && n < workinghours) {
+			n++;
+			newcase.EmpWageUC4();
 		}
-		System.out.println("Total employee wage is" + totalempwage );
 	}
+	public static void main(String[] args) {
+		EmpWageUC6 days=new EmpWageUC6();
+		days.EmpWageUC6();
+
+	}
+
 
 }
-
-
